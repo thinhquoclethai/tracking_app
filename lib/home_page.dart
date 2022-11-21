0f086f51e29ba14a56cf97173c9389f77968adc6
dungngminh <63831488+dungngminh@users.noreply.github.com>
2022-11-22 01:44:29 +0700

    add audio

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -14,25 +16,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  /// mode = 1 -> Duong do thi
   ///
-  /// mode = 2 -> Duong cao toc
-  int mode = 1;
   String speedWarningMessage = 'Không có cảnh báo nguy hiểm';
   String distanceWarningMessage = 'Không có cảnh báo nguy hiểm';
+  bool _isPlayerPlaying = false;
+  bool _isUserClick = false;
 
   late final WebSocketChannel _channelSpeed;
-  late final WebSocketChannel _channelDistance;
+  late final AudioPlayer _audioPlayer;
 
   @override
   void initState() {
     super.initState();
+    _audioPlayer = AudioPlayer();
+
     _channelSpeed = WebSocketChannel.connect(Uri.parse('ws://${widget.ip}'));
-    _channelDistance = WebSocketChannel.connect(Uri.parse('ws://${widget.ip}'));
+    initAudioSource();
+  }
+
+  Future<void> initAudioSource() async {
+    await _audioPlayer.setAsset('assets/effect.mp3');
+    await _audioPlayer.setLoopMode(LoopMode.all);
   }
 
   @override
   void dispose() {
+    _audioPlayer.dispose();
     super.dispose();
   }
 
@@ -49,192 +58,116 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 32,
-            ),
-            Text(
-              'Vận tốc',
-              style: theme.textTheme.displayLarge
-                  ?.copyWith(color: theme.colorScheme.onSurface),
-            ),
-            StreamBuilder(
-              stream: _channelSpeed.stream,
-              builder: (context, snapshot) {
-                var speed = '';
-                if (snapshot.hasData) {
-                  try {
-                    final data = snapshot.data as String;
-                    final speedDistance =
-                        data.substring(data.length - 7, data.length);
-                    if (int.tryParse(speedDistance.split(' ')[1]) != null) {
-                      speed = speedDistance.split(' ')[1];
-                    }
-                  } catch (e) {}
-                }
+      body: Column(
+        children: [
+          const Spacer(),
+          Text(
+            'Vận tốc',
+            style: theme.textTheme.displayLarge
+                ?.copyWith(color: theme.colorScheme.onSurface),
+          ),
+          const SizedBox(height: 8),
+          StreamBuilder(
+            stream: _channelSpeed.stream,
+            builder: (context, snapshot) {
+              var speed = '';
+              if (snapshot.hasData) {
+                try {
+                  final data = snapshot.data as String;
+                  final speedDistance =
+                      data.substring(data.length - 3, data.length);
 
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  if (mode == 1) {
-                    if (speed.isNotEmpty && int.parse(speed) >= 30) {
-                      setState(() {
-                        speedWarningMessage = 'Tốc độ đang ở mức nguy hiểm';
-                      });
-                    } else {
-                      setState(() {
-                        speedWarningMessage = 'Không có cảnh báo nguy hiểm';
-                      });
-                    }
-                  } else {
-                    if (speed.isNotEmpty && int.parse(speed) >= 50) {
-                      setState(() {
-                        speedWarningMessage = 'Tốc độ đang ở mức nguy hiểm';
-                      });
-                    } else {
-                      setState(() {
-                        speedWarningMessage = 'Không có cảnh báo nguy hiểm';
-                      });
-                    }
-                  }
-                });
-                return Text(
-                  '${speed.isEmpty ? 0 : speed} km/h',
-                  style: const TextStyle(
-                    fontSize: 57,
-                    height: 64 / 57,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              speedWarningMessage,
-              style: const TextStyle(
-                fontSize: 16,
-                height: 24 / 16,
-              ),
-            ),
-            const SizedBox(
-              height: 36,
-            ),
-            Text(
-              'Khoảng cách',
-              style: theme.textTheme.displayMedium
-                  ?.copyWith(color: theme.colorScheme.onSurface),
-            ),
-            StreamBuilder(
-              stream: _channelDistance.stream,
-              builder: (context, snapshot) {
-                var distance = '';
-                if (snapshot.hasData) {
-                  try {
-                    final data = snapshot.data as String;
-                    final speedDistance =
-                        data.substring(data.length - 7, data.length);
-                    if (int.tryParse(speedDistance.split(' ')[0]) != null) {
-                      distance = speedDistance.split(' ')[0];
-                    }
-                  } catch (e) {}
-                }
+                  speed = speedDistance;
+                } catch (e) {}
+              }
 
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  if (mode == 1) {
-                    if (distance.isNotEmpty && int.parse(distance) < 20) {
-                      setState(() {
-                        distanceWarningMessage = 'Khoảng cách nguy hiểm';
-                      });
-                    } else {
-                      setState(() {
-                        distanceWarningMessage = 'Không có cảnh báo nguy hiểm';
-                      });
-                    }
-                  } else {
-                    if (distance.isNotEmpty && int.parse(distance) < 30) {
-                      setState(() {
-                        distanceWarningMessage = 'Khoảng cách nguy hiểm';
-                      });
-                    } else {
-                      setState(() {
-                        distanceWarningMessage = 'Không có cảnh báo nguy hiểm';
-                      });
-                    }
-                  }
-                });
-                return Text(
-                  '${distance.isEmpty ? 0 : distance} cm',
-                  style: const TextStyle(
-                    fontSize: 57,
-                    height: 64 / 57,
-                  ),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              distanceWarningMessage,
-              style: const TextStyle(
-                fontSize: 16,
-                height: 24 / 16,
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  Text(
-                    'Chế độ',
-                    style: theme.textTheme.titleLarge,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: DropdownButtonFormField<int>(
-                value: mode,
-                decoration: const InputDecoration(border: OutlineInputBorder()),
-                items: const [
-                  DropdownMenuItem(
-                    value: 1,
-                    child: Text('Đường đồ thị'),
-                  ),
-                  DropdownMenuItem(
-                    value: 2,
-                    child: Text('Đường cao tốc'),
-                  ),
-                ],
-                onChanged: (value) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                if (speed.isNotEmpty && int.parse(speed) >= 50) {
                   setState(() {
-                    mode = value ?? 0;
-                    log(mode.toString(), name: 'MODE');
+                    speedWarningMessage = 'Tốc độ đang ở mức nguy hiểm';
                   });
-                },
-              ),
+                  if (!_isPlayerPlaying && !_isUserClick) {
+                    _audioPlayer.play();
+                    log('Play');
+                    setState(() {
+                      _isPlayerPlaying = true;
+                    });
+                  }
+                } else {
+                  setState(() {
+                    speedWarningMessage = 'Không có cảnh báo nguy hiểm';
+                  });
+                  if (_isPlayerPlaying) {
+                    _audioPlayer.pause();
+                    log('pause');
+
+                    setState(() {
+                      _isPlayerPlaying = false;
+                    });
+                  }
+                }
+              });
+              return Text(
+                '${speed.isEmpty ? 0 : speed} km/h',
+                style: const TextStyle(
+                  fontSize: 57,
+                  height: 64 / 57,
+                ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            speedWarningMessage,
+            style: TextStyle(
+              fontSize: 24,
+              height: 32 / 24,
+              color: Theme.of(context).colorScheme.error,
             ),
-            const SizedBox(
-              height: 24,
+          ),
+          const SizedBox(
+            height: 48,
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: const Text('Gọi cấp cứu'),
+                  onPressed: () {
+                    launchUrl(Uri(scheme: 'tel', path: '115'));
+                  },
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (_isPlayerPlaying) {
+                      setState(() {
+                        _isPlayerPlaying = false;
+                        _isUserClick = true;
+                      });
+                      _audioPlayer.pause();
+
+                      return;
+                    }
+                    setState(() {
+                      _isPlayerPlaying = true;
+                      _isUserClick = false;
+                    });
+                    _audioPlayer.play();
+                  },
+                  child: Text(
+                    _isPlayerPlaying ? 'Dừng cảnh báo' : 'Thử cảnh báo',
+                  ),
+                )
+              ],
             ),
-            Center(
-              child: ElevatedButton(
-                child: const Text('Gọi cấp cứu'),
-                onPressed: () {
-                  launchUrl(Uri(scheme: 'tel', path: '115'));
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+        ],
       ),
     );
   }
